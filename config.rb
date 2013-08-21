20c7dbd282d7fe8a565d0dd2e4596e9931054862
Kentaro Kuribayashi <kentarok@gmail.com>
2013-08-21 14:53:16 +0900

    middleman-deployを導入

--- config.rb
+++ config.rb
@@ -68,3 +68,10 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method       = :git
+  deploy.remote       = 'origin'
+  deploy.branch       = 'master'
+end
